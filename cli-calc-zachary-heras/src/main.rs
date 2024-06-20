70b5b2c4a5013dd3402c06aca7ff25d79ece22b0
Zachary Heras <zacharyheras@gmail.com>
2024-06-19 23:51:26 -0400

    Added unit tests. Finished

--- cli-calc-zachary-heras/src/main.rs
+++ cli-calc-zachary-heras/src/main.rs
@@ -1,56 +1,6 @@
-use clap::{Command, Arg};
+use clap::{Arg, Command};
 
-fn main() {
-    // I tried for hours to use the "derive" version of clap with no success
-    let matches = Command::new("CLI Calculator")
-        .version("0.1.0")
-        .about("This program is a command line interface calculator that works with integers")
-        .arg(
-            Arg::new("operand1")
-                .help("First operand")
-                .short('1')
-                .required(false)
-                .requires("operator")
-                .requires("operand1")
-        )
-        .arg(
-            Arg::new("operator")
-                .help("Operator")
-                .short('o')
-                .required(false)
-                .requires("operand1")
-                .requires("operand2")
-        )
-        .arg(
-            Arg::new("operand2")
-                .help("Second operand")
-                .short('2')
-                .required(false)
-                .requires("operator")
-                .requires("operand2")
-        )
-        .subcommand(
-            Command::new("--about")
-                .about("Displays details about author and program")
-        )
-        .get_matches();
-
-    if matches.subcommand_matches("--about").is_some() {
-        println!("About stuff here.")
-    }
-
-    // I do not understand why ::<String> is needed
-    // If it is not included, cargo says that clone trait is needed, which is not possible with
-    // &str. So, I am not sure how I can convert a argument to a &str directly
-    // What is unwrap()?
-    // How can I assign optionally
-    let operand1_str: &str = matches.get_one::<String>("operand1").unwrap();
-    let operator_str: &str = matches.get_one::<String>("operator").unwrap();
-    let operand2_str: &str = matches.get_one::<String>("operand2").unwrap();
-
-    let operand1: i32 = operand1_str.parse().expect("Invalid operand; Must be i32");
-    let operand2: i32 = operand2_str.parse().expect("Invalid operand; Must be i32");
-
+fn calculate(operand1: i32, operand2: i32, operator_str: &str) -> i32 {
     let result: i32 = match operator_str {
         "+" | "add" => operand1 + operand2,
         "-" | "sub" => operand1 - operand2,
@@ -62,10 +12,97 @@ fn main() {
             operand1 / operand2
         }
         _ => {
-            eprintln!("Invalid operator: {}", operator_str);
-            return;
+            // panic here or return none? I chose to panic
+            panic!("Invalid operator: {}", operator_str);
         }
     };
 
-    println!("{}", result);
+    return result;
+}
+
+fn main() {
+    let matches = Command::new("CLI Calculator")
+        .version("0.1.0")
+        .about("This program is a command line interface calculator that works with integers")
+        .arg(
+            Arg::new("operand1")
+                .help("First operand")
+                .short('1')
+                .required(false)
+                .requires("operator")
+                .requires("operand1"),
+        )
+        .arg(
+            Arg::new("operator")
+                .help("Operator")
+                .short('o')
+                .required(false)
+                .requires("operand1")
+                .requires("operand2"),
+        )
+        .arg(
+            Arg::new("operand2")
+                .help("Second operand")
+                .short('2')
+                .required(false)
+                .requires("operator")
+                .requires("operand2"),
+        )
+        .subcommand(Command::new("--about").about("Displays details about author and program"))
+        .get_matches();
+
+    if matches.subcommand_matches("--about").is_some() {
+        println!("Made by Zachary Heras. This program is an integer CLI calculator.")
+    } else {
+        let operand1_str: &str = matches.get_one::<String>("operand1").unwrap();
+
+        let operand2_str: &str = matches.get_one::<String>("operand2").unwrap();
+
+        let operator_str: &str = matches.get_one::<String>("operator").unwrap();
+
+        let operand1: i32 = operand1_str.parse().expect("Invalid operand; Must be i32");
+        let operand2: i32 = operand2_str.parse().expect("Invalid operand; Must be i32");
+
+        let result = calculate(operand1, operand2, operator_str);
+
+        println!("{}", result);
+    }
+}
+
+
+#[test]
+fn test_calculate_add() {
+    assert!(calculate(2, 5, "add") == 7);
+    assert!(calculate(2, 5, "+") == 7);
+}
+
+#[test]
+fn test_calculate_sub() {
+    assert!(calculate(2, 5, "sub") == -3);
+    assert!(calculate(2, 5, "-") == -3);
+}
+
+#[test]
+fn test_calculate_mul() {
+    assert!(calculate(2, 5, "mul") == 10);
+    assert!(calculate(2, 5, "*") == 10);
+}
+
+#[test]
+fn test_calculate_div() {
+    assert!(calculate(2, 5, "div") == 0);
+    assert!(calculate(2, 5, "/") == 0);
+}
+
+#[test]
+#[should_panic(expected = "Division by zero")]
+fn test_calculate2() {
+    calculate(4, 0, "div");
+    calculate(4, 0, "/");
+}
+
+#[test]
+#[should_panic(expected = "Invalid operator: hello world")]
+fn test_calculate3() {
+    calculate(1, 2, "hello world");
 }
